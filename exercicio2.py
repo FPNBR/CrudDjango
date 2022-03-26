f932b1d97f4b99db95a66045a7afbeb3ff7d056b
FPNBR <felipepny@gmail.com>
2022-03-25 21:37:19 -0300

    Deletado exercícios

--- exercicio2.py
+++
@@ -1,9 +0,0 @@
-# Programa para dizer se a letra que o usuário digitou é uma vogal ou não
-
-vogal = input('Digite uma letra: ')
-
-if vogal == 'a' or vogal == 'e' or vogal == 'i' or vogal == 'o' or vogal == 'u' or \
-        vogal == 'A' or vogal == 'E' or vogal == 'I' or vogal == 'O' or vogal == 'U':
-    print("Você digitou uma vogal: " + vogal)
-else:
-    print("Você digitou uma Consoante: " + vogal)
